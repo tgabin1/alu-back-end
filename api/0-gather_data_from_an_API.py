647a2e3e7de71376e08c5ff4a7aa9beb273f93d1
tgabin1 <t.ishimwe@alustudent.com>
2025-07-21 21:09:29 +0200

    changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""
-getting data using api
-"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    employee_Id = int(sys.argv[1])
+    employee_id = int(sys.argv[1])
 
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    user_data_url = "https://jsonplaceholder.typicode.com/users"
+    # Get employee name
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_data = requests.get(user_url).json()
+    employee_name = user_data.get("name")
 
-    user_response = requests.get(user_data_url)
-    todo_response = requests.get(todo_url)
-    # if todo_response.status_code & user_response.status_code == 200:
-    todos = todo_response.json()
-    users = user_response.json()
-    for user in users:
-        if user.get("id") == employee_Id:
-            employee_name = user.get("name")
-    # filter completed tasks
-    done = []
-    total = 0
-    completed = 0
-    for todo in todos:
-        if todo.get("userId") == employee_Id:
-            total += 1
-            if todo.get("completed"):
-                completed += 1
-                done.append(todo.get("title"))
-    # Display the progress information
-    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
-    for _ in done:
-        print(f"\t {_}")
+    # Get tasks
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todo_data = requests.get(todo_url).json()
+    total_tasks = len(todo_data)
+    done_tasks = [task for task in todo_data if task.get("completed")]
+
+    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
